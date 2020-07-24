6571e32b50d4ad415e4a44252a30ecad6adbe221
a.romanov <a.romanov@rbkmoney.com>
2020-07-24 11:56:04 +0300

    BJ-964: Switch Wallet to Kafka

--- src/main/java/com/rbkmoney/fistful/magista/poller/handler/WalletEventHandler.java
+++
@@ -1,7 +0,0 @@
-package com.rbkmoney.fistful.magista.poller.handler;
-
-import com.rbkmoney.fistful.wallet.Change;
-import com.rbkmoney.fistful.wallet.SinkEvent;
-
-public interface WalletEventHandler extends EventHandler<Change, SinkEvent> {
-}
