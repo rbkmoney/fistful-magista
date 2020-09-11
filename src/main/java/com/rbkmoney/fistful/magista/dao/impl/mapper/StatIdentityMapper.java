3f9aef6ad11b46e86616c3fd1716ecad420e5617
karleowne <a.karlov@rbkmoney.com>
2020-09-11 15:02:56 +0700

    bump proto

--- src/main/java/com/rbkmoney/fistful/magista/dao/impl/mapper/StatIdentityMapper.java
+++ src/main/java/com/rbkmoney/fistful/magista/dao/impl/mapper/StatIdentityMapper.java
@@ -20,7 +20,7 @@ public class StatIdentityMapper implements RowMapper<Map.Entry<Long, StatIdentit
         identity.setId(rs.getString(IDENTITY_DATA.IDENTITY_ID.getName()));
         identity.setName(rs.getString(IDENTITY_DATA.NAME.getName()));
         identity.setCreatedAt(TypeUtil.temporalToString(rs.getObject(IDENTITY_DATA.CREATED_AT.getName(), LocalDateTime.class)));
-        identity.setProvider(rs.getInt(IDENTITY_DATA.IDENTITY_PROVIDER_ID.getName()));
+        identity.setProvider(rs.getString(IDENTITY_DATA.IDENTITY_PROVIDER_ID.getName()));
         identity.setIdentityClass(rs.getString(IDENTITY_DATA.IDENTITY_CLASS_ID.getName()));
         identity.setIdentityLevel(rs.getString(IDENTITY_DATA.IDENTITY_LEVEL_ID.getName()));
         identity.setEffectiveChallenge(rs.getString(IDENTITY_DATA.IDENTITY_EFFECTIVE_CHALLENGE_ID.getName()));
